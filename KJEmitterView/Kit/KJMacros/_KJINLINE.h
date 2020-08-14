ac3ca6ad5144623e26b0218c31c879149767da41
77。 <393103982@qq.com>
2020-08-14 18:12:46 +0800

    Add 5.0.4

--- KJEmitterView/Kit/KJMacros/_KJINLINE.h
+++ KJEmitterView/Kit/KJMacros/_KJINLINE.h
@@ -21,27 +21,11 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
 /// 字符串转换为非空
-NS_INLINE NSString * kString(NSString *string){
+NS_INLINE NSString * kEmptyString(NSString *string){
     return (string ?: @"");
 }
-/// 字符串是否为空
-NS_INLINE bool kEmptyIsString(NSString *string){
-    return ([string isKindOfClass:[NSNull class]] || string == nil || ([string length] < 1 ? true:false));
-}
-/// 数组是否为空
-NS_INLINE bool kEmptyIsArray(NSArray *array){
-    return (array == nil || [array isKindOfClass:[NSNull class]] || array.count == 0);
-}
-/// 字典是否为空
-NS_INLINE bool kEmptyIsDictionary(NSDictionary *dict){
-    return (dict == nil || [dict isKindOfClass:[NSNull class]] || dict.allKeys == 0);
-}
-/// 对象是否为空
-NS_INLINE bool kEmptyIsObject(NSObject *_object){
-    return (_object == nil || [_object isKindOfClass:[NSNull class]] || ([_object respondsToSelector:@selector(length)] && [(NSData*)_object length] == 0) || ([_object respondsToSelector:@selector(count)] && [(NSArray*)_object count] == 0));
-}
 /// 随机颜色
-NS_INLINE UIColor * kRandomColor(void){
+NS_INLINE UIColor * kRandomColor(){
     return [UIColor colorWithRed:((float)arc4random_uniform(256)/255.0) green:((float)arc4random_uniform(256)/255.0) blue:((float)arc4random_uniform(256)/255.0) alpha:1.0];
 }
 /// 交换方法的实现
@@ -70,7 +54,7 @@ NS_INLINE void kAlertViewAutoDismiss(NSString *message, CGFloat delay){
     dispatch_async(dispatch_get_main_queue(), ^{
         UIAlertView *alerView = [[UIAlertView alloc] initWithTitle:@"提示" message:message delegate:nil cancelButtonTitle:nil otherButtonTitles:nil, nil];
         [alerView show];
-        [alerView performSelector:@selector(dismissWithClickedButtonIndex:animated:) withObject:@[@0,@1] afterDelay:delay];
+        [alerView performSelector:@selector(dismissWithClickedButtonIndex:animated:) withObject:@[@0, @1] afterDelay:delay];
     });
 }
 /// 系统加载动效
@@ -113,6 +97,12 @@ NS_INLINE UITableViewCell * kCellSubviewTableview(UIView *subview, UITableView *
     NSIndexPath *indexPath = [tableview indexPathForRowAtPoint:subviewFrame.origin];
     return [tableview cellForRowAtIndexPath:indexPath];
 }
+/// 强制让App直接退出（非闪退，非崩溃）
+NS_INLINE void kExitApplication(NSTimeInterval duration,void(^block)(void)) {
+    [UIView animateWithDuration:duration animations:block completion:^(BOOL finished) {
+        exit(0);
+    }];
+}
 #pragma mark -------------- GCD 线程处理 -------------
 /// 主线程
 NS_INLINE void kGCD_main(dispatch_block_t block) {
@@ -124,27 +114,28 @@ NS_INLINE void kGCD_main(dispatch_block_t block) {
 }
 /// 子线程
 NS_INLINE void kGCD_async(dispatch_block_t block) {
-    dispatch_queue_t queue = dispatch_queue_create("com.yangkejun.gcd", DISPATCH_QUEUE_CONCURRENT);
+//    dispatch_queue_t queue = dispatch_queue_create("com.yangkejun.gcd", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_async(queue, block);
 }
-/// 线程异步并行队列
-NS_INLINE void kGCD_group_notify(dispatch_block_t block,dispatch_block_t notify) {
-    dispatch_queue_t queue = dispatch_queue_create("com.yangkejun.gcd", DISPATCH_QUEUE_CONCURRENT);
+/// 异步并行队列，携带可变参数（需要nil结尾）
+NS_INLINE void kGCD_group_notify(dispatch_block_t notify,dispatch_block_t block,...) {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_group_t group = dispatch_group_create();
     dispatch_group_async(group, queue, block);
-    dispatch_group_notify(group, queue, ^{ dispatch_async(dispatch_get_main_queue(), notify); });
-}
-/// 延时执行
-NS_INLINE void kGCD_after(int64_t delayInSeconds, dispatch_block_t block) {
-    dispatch_queue_t queue = dispatch_queue_create("com.yangkejun.gcd", DISPATCH_QUEUE_CONCURRENT);
-    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(time, queue, block);
+    va_list args;dispatch_block_t arg;
+    va_start(args, block);
+    while ((arg = va_arg(args, dispatch_block_t))) {
+        dispatch_group_async(group, queue, arg);
+    }
+    va_end(args);
+    dispatch_group_notify(group, queue, notify);
 }
 /// 栅栏
-NS_INLINE dispatch_queue_t kGCD_barrier(dispatch_block_t block, dispatch_block_t barrier) {
-    dispatch_queue_t queue = dispatch_queue_create("com.yangkejun.gcd", DISPATCH_QUEUE_CONCURRENT);
+NS_INLINE dispatch_queue_t kGCD_barrier(dispatch_block_t block,dispatch_block_t barrier) {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_async(queue, block);
-    dispatch_barrier_async(queue, barrier);
+    dispatch_barrier_async(queue, ^{ dispatch_async(dispatch_get_main_queue(), barrier); });
     return queue;
 }
 /// 一次性
@@ -152,10 +143,27 @@ NS_INLINE void kGCD_once(dispatch_block_t block) {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, block);
 }
+/// 延时执行
+NS_INLINE void kGCD_after(int64_t delayInSeconds, dispatch_block_t block) {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(time, queue, block);
+}
 /// 快速迭代
-NS_INLINE void kGCD_apply(int iterations, void (^block)(size_t)) {
-    dispatch_queue_t queue = dispatch_queue_create("com.yangkejun.gcd", DISPATCH_QUEUE_CONCURRENT);
+NS_INLINE void kGCD_apply(int iterations, void(^block)(size_t idx)) {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_apply(iterations, queue, block);
+}
+/// 计时器
+static dispatch_source_t timer = nil;
+NS_INLINE dispatch_source_t kGCD_timer(int64_t delayInSeconds, dispatch_block_t block) {
+    if (timer) dispatch_source_cancel(timer);
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    dispatch_source_set_timer(timer, dispatch_walltime(nil, 0), delayInSeconds * NSEC_PER_SEC, 0);
+    dispatch_source_set_event_handler(timer, block);
+    dispatch_resume(timer);
+    return timer;
 }
 
 #pragma clang diagnostic pop
